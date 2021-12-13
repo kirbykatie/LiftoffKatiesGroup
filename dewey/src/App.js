810d3dbc01f1acc9dcf21faf5eb191f1c5d0819a
SayJinx <whatsupguest@gmail.com>
2021-12-13 12:41:38 -0600

    Set up basic card in UserLogIn.

--- dewey/src/App.js
+++ dewey/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
+import UserLogIn from './components/UserLogIn/UserLogIn';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 
@@ -9,6 +10,7 @@ function App() {
       <div>
       <NavBar/>
       <LandingPage/>
+      <UserLogIn />
       </div>
   );
 }
---
+++ dewey/src/components/UserLogIn/UserLogIn.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+export default function UserLogIn() {
+
+    return (
+        <div>
+            <Card elevation={4}>
+                <CardHeader title="My Dewey Library Card" />
+            </Card>
+            <CardContent>
+ 
+            </CardContent>
+        </div>
+    )
+}
