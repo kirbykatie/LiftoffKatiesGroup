2c92c036fa61a20da62f6ea6dc95ec4bd97b9b7c
Kenya Caldwell <kenya78hami@gmail.com>
2021-12-12 21:49:48 -0600

    add navbar component

--- dewey/src/App.js
+++ dewey/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
+import {BrowserRouter as Router} from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <header>
+          <NavBar/>
+        </header>
       < LandingPage />
+      </Router>
     </div>
   );
 }
