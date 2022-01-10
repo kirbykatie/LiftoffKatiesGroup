b038f872dd801ec7fe13d1df5227fdd5bd05579c
Dava Lincoln <lincolndja@gmail.com>
2022-01-09 19:50:33 -0600

    ready to push

--- dewey/src/components/LandingPage/LandingPage.js
+++ dewey/src/components/LandingPage/LandingPage.js
@@ -2,18 +2,24 @@ import React from 'react';
 import './LandingPage.css';
 
 
+
 function LandingPage () {
     return (
         <div>
-            <header className= 'Header'> fill in </header>
+            <header className= 'Header'>
+
+            </header>
            <h1 classname= 'Banner'>Welcome To Dewey!</h1>
            <div className='Grid-container'>
             <div className='Grid-child-left'>
-             insert dewey logo 
-            </div>
+             <img src="Assests/FullLogo.png" alt='Dewey Logo'></img>
+            </div> 
             <div className='Grid-child-right'>
-             About us info 
+             Dewey is the brain child of four beginning developers. This app lets you easily organize and manage your book collection. It even has the ability to diplay them on a "Bookshelf" so you can show off that awesome cover artwork. When you can't remeber if you already have that book, Dewey does!
             </div>
+           </div>
+           <div>
+               App Features
            </div>
         </div>
 
