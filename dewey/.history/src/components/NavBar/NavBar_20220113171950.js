b98eab22d18391bd7377e9e9573e88d80b2c139e
Kenya Caldwell <kenya78hami@gmail.com>
2022-01-13 17:23:44 -0600

    add routing all links work except search

---
+++ dewey/.history/src/components/NavBar/NavBar_20220113171950.js
@@ -0,0 +1,43 @@
+import { Toolbar, Typography, AppBar} from "@material-ui/core";
+import {Link} from 'react-router-dom';
+
+
+function NavBar() {
+    return (
+        <div>
+            {/* <AppBar>
+                <Toolbar>
+                    <Typography>
+                <nav>
+                    <Link to='Userlogin'>Login</Link>
+                    <Link to='UserSignUp'>Sign Up</Link>
+                </nav>
+                 </Typography>
+                </Toolbar>
+            </AppBar>
+        conditional links if logged in */}
+        {/* <AppBar>
+                <Toolbar>        
+                    <Typography>  */}
+                         <nav>
+                            <li>
+                            <Link to='/landingPage'>Home</Link>
+                            </li>
+                            <li>
+                            <Link to='/userLogin'>Login</Link>
+                            </li>
+                            <li>
+                            <Link to='/search'>Search</Link>
+                            </li>
+                            <Link to='/bookShelf'>BookShelf</Link>
+                        </nav>
+                    {/* </Typography>
+                 </Toolbar>
+            </AppBar> */}
+
+        </div>
+    )
+}
+
+export default NavBar
+
