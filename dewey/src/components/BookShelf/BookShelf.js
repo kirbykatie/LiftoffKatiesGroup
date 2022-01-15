44bcd774aff6c491fd8a6478e0d1e8c7de9bd2fc
Katy Schoenborn <katyhooper1@gmail.com>
2022-01-14 22:23:07 -0600

    Added code to post to api.

--- dewey/src/components/BookShelf/BookShelf.js
+++ dewey/src/components/BookShelf/BookShelf.js
@@ -27,6 +27,22 @@ function List() {
       </Grid>
       </Grid>
       </Container>
+
+      let req = new XMLHttpRequest();
+
+      req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+          console.log(req.responseText);
+          }
+      };
+
+      req.open("PUT", "https://api.jsonbin.io/v3/b/61db29cb2675917a628d0fdd/1>", true);
+      req.setRequestHeader("Content-Type", "application/json");
+      req.setRequestHeader("X-Master-Key", "$2b$10$/XJZaP2..00.uopLhP97xeoEIvOv4ReDDGram2cdcML7i6Teyakoy");
+      req.send("title": {new.title});
+      req.send("author": {new.title});
+      req.send("isbn": {new.isbn});
+
     </div>
   );
 }
