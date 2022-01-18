955846594a7dd358ac351ae189fb3df2b028c671
SayJinx <whatsupguest@gmail.com>
2022-01-18 10:18:15 -0600

    beginning routing for login and sign up

--- dewey/src/App.js
+++ dewey/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import UserLogIn from './components/UserLogIn/UserLogIn';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
+      <Router>
       <div>
       <NavBar/>
       <LandingPage/>
       <UserLogIn />
       </div>
+      </Router>
   );
 }
 
